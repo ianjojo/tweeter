15fbc2f7e06c53159a72f8711bd54d08df53a1ae
Ian Johnston <ianallanjohnston@gmail.com>
2022-12-01 11:13:23 -0800

    whoops

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,8 +7,6 @@ export default function App({
   pageProps: { session, ...pageProps },
 }) {
   return (
-    // `session` comes from `getServerSideProps` or `getInitialProps`.
-    // Avoids flickering/session loading on first load.
     <SessionProvider session={session}>
       <RecoilRoot>
         <Component {...pageProps} />
